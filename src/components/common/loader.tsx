acaefeffbd5faa6ba8f44c56c4c8bc0290aee729
Kyle <capriok7@gmail.com>
2021-02-10 18:05:42 -0700

    init

--- src/components/common/loader.tsx
+++ src/components/common/loader.tsx
@@ -6,16 +6,14 @@ interface Props {
 
 }
 
-const Loader: React.FC<Props> = ({ }) => {
-	return (
-		<div className="wrapper">
-			<div className="loading">
-				<span className="loader"></span>
-				<span className="loader"></span>
-				<span className="loader"></span>
-			</div>
+const Loader: React.FC<Props> = ({ }) => (
+	<div className="wrapper">
+		<div className="loading">
+			<span className="loader"></span>
+			<span className="loader"></span>
+			<span className="loader"></span>
 		</div>
-	)
-}
+	</div>
+)
 
 export default Loader
